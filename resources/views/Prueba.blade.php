788aace88047954a4ab83cfcfd869fd1db23f333
Xavied <64937713+Xavied@users.noreply.github.com>
2020-07-03 13:13:19 -0500

    Probando Rama

---
+++ resources/views/Prueba.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('font/css/open-iconic-bootstrap.min.css')}}">
+    <title>Guia hoteles</title>
+</head>
+
+<body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{asset('jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('popper.js/dist/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+
+
+
+    <div class="container">
+        <h1>Hola Mundo</h1>
+    </div>
+
+
+</body>
+
+</html>
