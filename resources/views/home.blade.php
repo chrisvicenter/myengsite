916b3caeb5adebdd648f414750fbae2a5ba49344
AO <alexortizperugachi@gmail.com>
2020-07-07 20:18:25 -0500

    se agregan imagenes para el carousel y los recuadros, tambien se modifica el estilo de las imagenes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="font/css/open-iconic-bootstrap.min.css">
     <link rel="shortcut icon" href="images/ninosICO.ico" />
-    <title>My EngSite</title>
+    <title>My EnG Site</title>
 </head>
 
 <body>
@@ -43,11 +43,9 @@
             </div>
         </div>
     </nav>
-
-    <br>
-    <div class="container">
+    <div class="container colorcntner shadow">
         <!--Breadcrumb página Home-->
-        <nav aria-label="breadcrumb">
+        <nav aria-label="breadcrumb " class="bodymrg rowtop">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item active"><a href="./index.html">Home</a></li>
             </ol>
@@ -60,16 +58,15 @@
                 <li data-target="#carouselControls" data-slide-to="1"></li>
                 <li data-target="#carouselControls" data-slide-to="2"></li>
             </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item itemimg active ">
-                    <img class="d-block w-100 imgcarousel" src="images/logofinalJPG.jpg" alt="Primer slide">
+            <div class="carousel-inner carouseltmn">
+                <div class="carousel-item text-center active ">
+                    <img src="images/book-1822474_1920.jpg" alt="Primer slide" class="imgtmncrsl ">
                 </div>
-                <div class="carousel-item itemimg">
-                    <img class="d-block w-100 imgcarousel" src="images/imgnav.jpg" alt="Segundo slide">
+                <div class="carousel-item text-center ">
+                    <img src="images/winter-1964361_1920.jpg" alt="Segundo slide" class="imgtmncrsl  ">
                 </div>
-                <div class="carousel-item itemimg  ">
-                    <img class="d-block w-100 imgcarousel" src="images/imglbo.jpg" alt="Tercer slide" >
-
+                <div class="carousel-item text-center">
+                    <img src="images/fantasy-2945514_1920.jpg" alt="Tercer slide" class="imgtmncrsl">
                 </div>
             </div>
             <!--Texto del carousel-->
@@ -78,9 +75,7 @@
                     <h2 class="text-center titlecapt">Welcome</h2>
                     <p class="txtcapt">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at risus nec neque tincidunt luctus vel finibus mi.
-                        Curabitur non tempor lorem, eget maximus nulla. Mauris venenatis ante non laoreet consequat. Nunc aliquam,
-                        quam finibus tristique elementum, leo mauris convallis est, ac rhoncus odio purus eget purus. Phasellus quis consequat lectus,
-                        et semper magna. Nam ultricies nulla neque, vitae hendrerit sem elementum ut.
+                        Curabitur non tempor lorem, eget maximus nulla. Mauris venenatis ante non laoreet consequat.
                     </p>
                 </div>
 
@@ -98,64 +93,74 @@
         </div>
         <!--fin del carousel-->
         <hr>
-        <div class="d-flex flex-wrap ">
-            <div class="row">
-                <div class="col d-flex flex-column align-items-start ">
-                    <img class="imgcont" src="images/imgcnto.jpg">
-                    <div class="caption d-none d-md-block d-flex ">
-                        <h4 class="ml-3 colorcont">Lorem ipsum</h4>
-                        <p class="ml-3 colorcont">Lorem ipsum dolor sit amet, consectetur</p>
+        <div class="d-flex flex-wrap bodymrg ">
+            <div class="carousel-inner">
+                <div class="row">
+                    <div class="col-sm-6  d-flex flex-column tmncont ">
+                        <div class="text-center">
+                            <img src="images/imgdrg.jpg" class="imgtmn">
+                        </div>
+                        <div class="caption d-none d-md-block d-flex ">
+                            <h5 class="ml-2 colorcont sizetlt">Lorem ipsum</h5>
+                            <p class="ml-2 colorcont sizecont">Lorem ipsum dolor sit amet, consectetur</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col d-flex flex-column align-items-start ">
-                    <img class="imgcont" src="images/imgoso.jpg">
-                    <div class="caption d-none d-md-block d-flex ">
-                        <h4 class="ml-3 colorcont">Lorem ipsum</h4>
-                        <p class="ml-3 colorcont">Lorem ipsum dolor sit amet, consectetur</p>
+                    <div class="col-sm-6  d-flex flex-column tmncont ">
+                        <div class="text-center">
+                            <img src="images/book-4133883_1280.jpg" class="imgtmn">
+                        </div>
+                        <div class="caption d-none d-md-block d-flex ">
+                            <h5 class="ml-2 colorcont sizetlt">Lorem ipsum</h5>
+                            <p class="ml-2 colorcont sizecont">Lorem ipsum dolor sit amet, consectetur</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="row rowtop">
-                <div class="col d-flex flex-column align-items-start ">
-                    <img class="imgcont" src="images/imgcnto.jpg">
-                    <div class="caption d-none d-md-block d-flex ">
-                        <h4 class="ml-3 colorcont">Lorem ipsum</h4>
-                        <p class="ml-3 colorcont">Lorem ipsum dolor sit amet, consectetur</p>
+            <div class="carousel-inner rowtop">
+                <div class="row">
+                    <div class="col-sm-6  d-flex flex-column tmncont">
+                        <div class="text-center">
+                            <img src="images/imgvndo.jpg" class="imgtmn">
+                        </div>
+                        <div class="caption d-none d-md-block d-flex ">
+                            <h5 class="ml-2 colorcont sizetlt">Lorem ipsum</h5>
+                            <p class="ml-2 colorcont sizecont">Lorem ipsum dolor sit amet, consectetur</p>
+                        </div>
+
                     </div>
-                </div>
-                <div class="col d-flex flex-column align-items-start ">
-                    <img class="imgcont" src="images/imgoso.jpg">
-                    <div class="caption d-none d-md-block d-flex ">
-                        <h4 class="ml-3 colorcont">Lorem ipsum</h4>
-                        <p class="ml-3 colorcont">Lorem ipsum dolor sit amet, consectetur</p>
+                    <div class="col-sm-6  d-flex flex-column tmncont ">
+                        <div class="text-center">
+                            <img src="images/imgastrn.jpg" class="imgtmn">
+                        </div>
+                        <div class="caption d-none d-md-block d-flex ">
+                            <h5 class="ml-2 colorcont sizetlt">Lorem ipsum</h5>
+                            <p class="ml-2 colorcont sizecont">Lorem ipsum dolor sit amet, consectetur</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <hr>
         <!--Footer de la página-->
-        <div class="foot">
+        <div class="foot bodymrg ">
             <div class="row">
                 <div class="col-sm-4 d-flex flex-column align-items-start ">
-                    <h3>About Web</h3>
-                    <hr style="border-color:white;">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    <h5>About Web</h5>
+                    <p class="sizecont">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Phasellus nec lorem eu risus imperdiet viverra consequat id lorem.
                         Curabitur vitae nisl in augue sodales porta. Ut vel sollicitudin dolor.</p>
                 </div>
                 <div class="col-sm-4 d-flex flex-column align-items-start">
-                    <h3>Principal Teacher</h3>
-                    <hr style="border-color:white;">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    <h5>Principal Teacher</h5>
+                    <p class="sizecont">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Phasellus nec lorem eu risus imperdiet viverra consequat id lorem.
                         Curabitur vitae nisl in augue sodales porta. Ut vel sollicitudin dolor.</p>
                 </div>
                 <div class="col-sm-4 d-flex flex-column align-items-start">
-                    <h3>Social Network</h3>
-                    <hr style="border-color:white;">
-                    <a href="https://www.instagram.com/ ">Instagram</a>
-                    <a href="https://www.facebook.com/ ">facebook</a>
-                    <a href="https://www.twitter.com/ ">Twitter</a>
+                    <h5>Social Network</h5>
+                    <a href="https://www.instagram.com/ " class="sizecont">Instagram</a>
+                    <a href="https://www.facebook.com/ " class="sizecont">facebook</a>
+                    <a href="https://www.twitter.com/ " class="sizecont">Twitter</a>
                 </div>
             </div>
         </div>
@@ -166,7 +171,7 @@
         <div class="container">
             <div class="row">
                 <div class="col d-flex flex-column align-items-start">
-                    <p>Tellme a story 2020 - NanoSoft100</p>
+                    <p>My EnG Site 2020 - NanoSoft100</p>
                 </div>
                 <div class="col d-flex flex-column align-items-end">
                     <p>Home&nbsp; &nbsp; &nbsp;Warning Legal
