9c680d86712b88eb3ebc4e0e1be8091f43ea1907
Christian Vicente <christianvicenter@hotmail.com>
2020-07-17 15:28:18 -0500

    Se creo la parte inicial del CRUD de posts

---
+++ resources/views/admin/posts/partials/form.blade.php
@@ -0,0 +1,28 @@
+<div class="form-group">
+    {{ Form::label('name', 'Name of unit') }}
+    {{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) }}
+</div>
+<div class="form-group">
+    {{ Form::label('slug', 'Friendly URL') }}
+    {{ Form::text('slug', null, ['class' => 'form-control', 'id' => 'slug']) }}
+</div>
+<div class="form-group">
+    {{ Form::label('body', 'Description') }}
+    {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+</div>
+<div class="form-group">
+    {{ Form::submit('Save', ['class' => 'btn btn-sm btn-primary']) }}
+</div>
+
+@section('scripts')
+<script src="{{ asset('vendor/stringToSlug/jquery.stringToSlug.min.js') }}"></script>
+<script>
+	$(document).ready(function(){
+	    $("#name, #slug").stringToSlug({
+	        callback: function(text){
+	            $('#slug').val(text);
+	        }
+	    });
+	});
+</script>
+@endsection
