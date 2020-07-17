2067526f47b01e68e4f44bc819ff7ba55b59560f
Christian Vicente <christianvicenter@hotmail.com>
2020-07-17 15:13:33 -0500

    Se completó el CRUD de Units

---
+++ resources/views/admin/units/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Edit unit
+                </div>
+
+                <div class="panel-body">
+                    {!! Form::model($unit, ['route' => ['units.update', $unit->id],
+                        'method'=>'PUT']) !!}
+                        
+                        @include('admin.units.partials.form')
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
