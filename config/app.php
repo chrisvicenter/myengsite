a95e6a86117328a42a5fc59fb365c2788ba9838c
Christian Vicente <christianvicenter@hotmail.com>
2020-07-24 12:25:15 -0500

    Se creo Cloudinary para imagenes, archivos aún en prueba

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
 
+        JD\Cloudder\CloudderServiceProvider::class,
+
     ],
 
     /*
@@ -232,7 +234,8 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Cloudder' => JD\Cloudder\Facades\Cloudder::class,
     ],
 
 ];
