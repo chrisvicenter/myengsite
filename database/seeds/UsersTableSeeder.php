1896359ea3ffac2645ad1a671d0c41bf192eca0c
Christian Vicente <58301860+chrisvicenter@users.noreply.github.com>
2020-08-11 12:00:16 -0500

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,23 +4,5 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        App\User::create([
-            'name' => 'Medium Usuario',
-            'email' => 'medi@correo.com',
-            'password' => bcrypt('123Nano_100')
-        ]);
-
-        App\User::create([
-            'name' => 'Viviana',
-            'email' => 'Vivi@correo.com',
-            'password' => bcrypt('teacherviviana011_V')
-        ]);
-    }
+    //
 }
