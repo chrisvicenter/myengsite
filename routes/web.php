23dad3993429f0069e8dd19f356341bf1ea6d4d8
Christian Vicente <christianvicenter@hotmail.com>
2020-09-26 22:47:42 -0500

    Se cambió la página principal

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('inicio');
+    return view('/home');
 });
+*/
+
+Route::get('/', 'HomeController@index');
 
 Route::get('/home', 'HomeController@index');
 //Se generar la vista para el aula virtual
