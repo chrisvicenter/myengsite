87f0d9045f2b4f7a3ccccb1fd2803c85a49dd811
Christian Vicente <christianvicenter@hotmail.com>
2021-01-24 22:41:55 -0500

    Corrección welcome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,8 +61,6 @@
             <p class="txtcapt">
                 My EnGSite se representa como una web lúdica y a la misma vez como una herramienta que desarrolla determinadas funciones, y que puede ser aplicada en el campo educativo
                 dentro de la enseñanza del idioma generando la participacion activa del estudiante, convirtiéndolo en un estudiante crítico y creativo.
-                Ectendiendo su aprendizaje a través de las actividades incluidas en sus clases como la creación de historias, aceptar retos de escritura,
-                describir y crear historias a partir de una imagen, etc. Usando herramientas colaborativas como el Blog creativo, la incorporación de podcast para mejorar la
             </p>
         </div>
     </div>
